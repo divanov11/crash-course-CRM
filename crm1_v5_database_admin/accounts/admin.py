fb207b8333d96ebccb2c625ce626dd0b452a4baa
divanov11 <33843378+divanov11@users.noreply.github.com>
2019-12-02 15:47:22 -0800

    Add files via upload

---
+++ crm1_v5_database_admin/accounts/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import *
+
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)
---
+++ crm1_v5_database_admin/accounts/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+# Create your models here.
+
+class Customer(models.Model):
+	name = models.CharField(max_length=200, null=True)
+	phone = models.CharField(max_length=200, null=True)
+	email = models.CharField(max_length=200, null=True)
+	date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+
+	def __str__(self):
+		return self.name
+
+class Product(models.Model):
+	CATEGORY = (
+			('Indoor', 'Indoor'),
+			('Out Door', 'Out Door'),
+			) 
+
+	name = models.CharField(max_length=200, null=True)
+	price = models.FloatField(null=True)
+	category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+	description = models.CharField(max_length=200, null=True)
+	date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+class Order(models.Model):
+	STATUS = (
+			('Pending', 'Pending'),
+			('Out for delivery', 'Out for delivery'),
+			('Delivered', 'Delivered'),
+			)
+
+	#customer = 
+	#product = 
+	date_created = models.DateTimeField(auto_now_add=True, null=True)
+	status = models.CharField(max_length=200, null=True, choices=STATUS)
