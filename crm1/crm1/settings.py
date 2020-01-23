a4182292a03e2a4a9595f6854bbef869552ebd43
divanov11 <33843378+divanov11@users.noreply.github.com>
2020-01-23 13:51:11 -0800

    Update settings.py

--- crm1/crm1/settings.py
+++ crm1/crm1/settings.py
@@ -169,33 +169,3 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = '*********'
 EMAIL_HOST_PASSWORD = '*********'
-
-
-
-
-#S3 BUCKETS CONFIG
-'''
-AWS_ACCESS_KEY_ID = '*****************'
-AWS_SECRET_ACCESS_KEY = '*****************'
-AWS_STORAGE_BUCKET_NAME = '*****************'
-
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-'''
-
-
-'''
-<?xml version="1.0" encoding="UTF-8"?>
-<CORSConfiguration xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
-<CORSRule>
-    <AllowedOrigin>*</AllowedOrigin>
-    <AllowedMethod>GET</AllowedMethod>
-    <AllowedMethod>POST</AllowedMethod>
-    <AllowedMethod>PUT</AllowedMethod>
-    <AllowedHeader>*</AllowedHeader>
-</CORSRule>
-</CORSConfiguration>
-
-'''
